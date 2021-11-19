a8483e1b8c4d90f0cd239f8eef0a9671e85c3ff8
hmeg2896 <hiroyuki.asai.407@gmail.com>
2021-11-20 00:02:09 +0900

    修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  
-  before_action :authenticate_user!, except: [:index, :show, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :select_prototype, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -12,7 +12,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+ 
     @comment = Comment.new
     @comments = @prototype.comments
   end
@@ -27,11 +27,11 @@ class PrototypesController < ApplicationController
   end
     
   def edit
-    @prototype = Prototype.find(params[:id])
+    
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
+    
       if  @prototype.update(prototype_params)
         redirect_to prototype_path
       else
@@ -46,18 +46,20 @@ class PrototypesController < ApplicationController
 
 
   private
-  
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index 
-    unless current_user == @prototype
-    redirect_to action: :index
-    end
   end
 
   def select_prototype
     @prototype = Prototype.find(params[:id])
   end
+
+  def move_to_index 
+    unless current_user.id == @prototype.user.id
+    redirect_to action: :index
+    end
+  end
+
+  
 end
